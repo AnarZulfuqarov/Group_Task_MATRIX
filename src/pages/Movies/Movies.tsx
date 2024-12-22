1fc2b4f9875591ef88f4cf3752793f20008b38f9
FullStackDeveloperAnar <anar.584@mail.ru>
2024-12-22 16:25:31 +0400

    movies-card

--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -1,7 +1,32 @@
+import { FaStar } from "react-icons/fa";
+import "./Movies.scss";
 const Movies = () => {
   return (
-    <div>Movies</div>
-  )
-}
+    <div className="container">
+      <div className="row movies pb-5 pt-5">
+        <div className="col-sm-6 col-md-4 col-lg-3 p-0  movie_card">
+          <div className="image">
+            <img src="https://via.placeholder.com/300" alt="movie" />
+          </div>
+          <div className="title">
+            <h1>Movie Title</h1>
+            <p>desc</p>
+            <div className="raiting">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+            <span>price</span>
+            <div className="text-center">
+              <button className="btn btn-danger">Book Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
