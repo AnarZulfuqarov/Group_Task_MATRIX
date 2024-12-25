d6226d1fae80a29673d835b9ff141ae8629755e1
FullStackDeveloperAnar <anar.584@mail.ru>
2024-12-25 18:32:52 +0400

    About Change

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,33 +1,42 @@
-import './About.scss'
+import "./About.scss";
 const About = () => {
   return (
-    <div className="about-us">
-      <div className="bg-image">
-
-</div>
-<img className='about-img' src="https://www.reliant.co.uk/blog/wp-content/uploads/2023/12/movies.jpg" alt="" />
-<p className='about-desc'>
-Welcome to UFA Cinema – Your Ultimate Movie Rental Destination!
-<br />
-<br />
-At UFA Cinema, we are passionate about bringing the magic of cinema straight to you. Whether you're a fan of the latest blockbusters, timeless classics, or hidden gems, our platform offers a wide variety of movies to suit every taste and occasion.
-<br />
-Why Choose UFA Cinema?
-<br />
-<br />
-
-Diverse Selection: From drama to action, comedy to romance, and everything in between, we’ve got it all!
-Convenience: Browse, select, and rent your favorite movies in just a few clicks.
-Accessibility: Enjoy seamless access to our platform from any device – be it your phone, tablet, or computer.
-High Quality: Experience movies in top-notch quality for the ultimate viewing experience.
-At UFA Cinema, we believe in creating moments of joy and entertainment for our customers. Our mission is to make movie rentals easy, enjoyable, and affordable for everyone.
-<br />
-Sit back, relax, and let UFA Cinema be your go-to destination for incredible movie experiences.
-
-
-</p>
+   <div className="row">
+     <div className="about-us col-12">
+      <div className="bg-image"></div>
+      <img
+        className="about-img"
+        src="https://www.reliant.co.uk/blog/wp-content/uploads/2023/12/movies.jpg"
+        alt=""
+      />
+      <p className="about-desc">
+        Welcome to UFA Cinema – Your Ultimate Movie Rental Destination!
+        <br />
+        <br />
+        At UFA Cinema, we are passionate about bringing the magic of cinema
+        straight to you. Whether you're a fan of the latest blockbusters,
+        timeless classics, or hidden gems, our platform offers a wide variety of
+        movies to suit every taste and occasion.
+        <br />
+        Why Choose UFA Cinema?
+        <br />
+        <br />
+        Diverse Selection: From drama to action, comedy to romance, and
+        everything in between, we’ve got it all! Convenience: Browse, select,
+        and rent your favorite movies in just a few clicks. Accessibility: Enjoy
+        seamless access to our platform from any device – be it your phone,
+        tablet, or computer. High Quality: Experience movies in top-notch
+        quality for the ultimate viewing experience. At UFA Cinema, we believe
+        in creating moments of joy and entertainment for our customers. Our
+        mission is to make movie rentals easy, enjoyable, and affordable for
+        everyone.
+        <br />
+        Sit back, relax, and let UFA Cinema be your go-to destination for
+        incredible movie experiences.
+      </p>
     </div>
-  )
-}
+   </div>
+  );
+};
 
-export default About
+export default About;
